dfb0c692bdb3e03289ef8d5ca896ec46808aaf05
Admin <hairais@mail.ru>
2024-08-15 20:35:01 +0700

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import { ArrowRight } from 'lucide-react';
 
+function ButtonUsual({ text }) {
+    return (
+        <button className="TopBar-ButtonUsual">
+            <i>{text}</i>
+        </button>
+    );
+}
+
+function ButtonArrow({ text }) {
+    return (
+        <button className="ButtonArrow">
+            <i>{text}</i>
+            <ArrowRight className="ButtonArrow-Arrow" />
+        </button>
+    );
+}
+
 function TopBar() {
     return (
         <div className="TopBar">
             <div className="TopBar-Navigation">
-                <button className="TopBar-ButtonUsual">Cards</button>
-                <button className="TopBar-ButtonUsual">About</button>
-                <button className="TopBar-ButtonUsual">App</button>
+                <ButtonUsual text="Cards" />
+                <ButtonUsual text="About" />
+                <ButtonUsual text="App" />
             </div>
             <div className="TopBar-Extra">
-                <button className="TopBar-ButtonUsual">Login</button>
-                <button className="TopBar-ButtonArrow">
-                    Apply now
-                    <ArrowRight />
-                </button>
+                <ButtonUsual text="Login" />
+                <ButtonArrow text="Apply Now" />
             </div>
         </div>);
 }
 
+function Introduction() {
+    return (
+        <div className="Introduction">
+            <div className="Introduction-LeftSide">
+                <h1>Making <span className="Introduction-Span">your</span> credit history.</h1>
+                <i className="Welcome">Welcome to credit, no experience necessary.</i>
+                <ButtonArrow text="Apply Now" />
+                <i className="Info">Variable APR range from 17.24% – 34.74%</i>
+            </div>
+            <div className="Introduction-RightSide">
+                <div className="Introduction-Card" />
+            </div>
+        </div>
+    )
+}
+
 export default function App() {
-    return (<div>
-        <TopBar />
-    </div>)
+    return (
+        <div>
+            <TopBar />
+            <Introduction />
+        </div>)
 }
